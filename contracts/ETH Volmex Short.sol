921925e4076bf6f8d613813e48d4c40fa38e30c0
Dipesh <me@dipeshsukhani.dev>
2021-03-12 14:48:43 +0800

    update for feedback v1

---
+++ contracts/ETH Volmex Short.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ETHVS is Ownable, ERC20 {
+    constructor() ERC20("ETH Volmex Short", "ETHVL") {}
+    
+    function mint(address _toWhom, uint256 amount) onlyOwner public {
+        _mint(_toWhom, amount);
+    }
+    
+    function burn(address _whose, uint256 amount) onlyOwner public {
+        _burn(_whose, amount);
+    }
+    
+    
+}
