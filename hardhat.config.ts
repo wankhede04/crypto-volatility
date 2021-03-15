cabe73cbf13f06c22a18246826b22599dd35806a
Derek Barrera <derekbarrera@gmail.com>
2021-03-14 20:29:54 -0600

    add typescript and typechain add basic deployment test

---
+++ hardhat.config.ts
@@ -0,0 +1,81 @@
+require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import 'hardhat-contract-sizer';
+import "hardhat-typechain";
+import 'hardhat-deploy';
+import * as dotenv from "dotenv";
+import "@nomiclabs/hardhat-etherscan";
+
+dotenv.config({ path: '.' + '/.env' });
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.getAddress());
+  }
+});
+
+export default {
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  },
+  // defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      loggingEnabled: true,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // same address and port for both Buidler and Ganache node
+      gas: 8000000,
+      gasLimit: 8000000,
+      gasPrice: 1,
+    },
+    kovan: {
+      url: "https://kovan.infura.io/v3/e2a0f0db1ef34ac69c77250c55f0b6fd", // same address and port for both Buidler and Ganache node
+      gas: 'auto',
+      gasPrice: 'auto'
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v5",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      4: '0xA296a3d5F026953e17F472B497eC29a5631FB51B', // but for rinkeby it will be a specific address
+      "goerli": '0x84b9514E013710b9dD0811c9Fe46b837a4A0d8E0', //it can also specify a specific netwotk name (specified in hardhat.config.js)
+    },
+    feeCollector: {
+      default: 1, // here this will by default take the second account as feeCollector (so in the test this will be a different account than the deployer)
+      1: '0xa5610E1f289DbDe94F3428A9df22E8B518f65751', // on the mainnet the feeCollector could be a multi sig
+      4: '0xa250ac77360d4e837a13628bC828a2aDf7BabfB3', // on rinkeby it could be another account
+    },
+
+  }
+};
---
+++ test/deployment.test.ts
@@ -0,0 +1,15 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Token contract", function() {
+  it("Deployment should assign the total supply of tokens to the owner", async function() {
+    const VolmexProtocol = await ethers.getContractFactory("VolmexProtocol");
+    const vp = await VolmexProtocol.deploy(0);
+
+    await vp.deployed();
+
+    console.log("VP deployed to:", vp.address);
+
+    expect(vp).to.not.equal(null);
+  });
+});
