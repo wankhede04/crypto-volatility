bd601b734aa71caceb6e2c392381b2b674159e7b
wankhede04 <wankhedevijay04@gmail.com>
2021-04-01 19:08:48 +0530

    Updated the name and symbol of position token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -32,9 +32,9 @@ async function main () {
   await dummyERC20Instance.deployed();
 
   // deploying the PositionTokenContracts
-  const ethvLongToken = await VolmexPositionTokenFactory.deploy('ETHVLong', 'ETHV');
+  const ethvLongToken = await VolmexPositionTokenFactory.deploy('ETHV', 'ETHV');
   await ethvLongToken.deployed();
-  const ethvShortToken = await VolmexPositionTokenFactory.deploy('ETHVShort', 'iETHV');
+  const ethvShortToken = await VolmexPositionTokenFactory.deploy('iETHV', 'iETHV');
   await ethvShortToken.deployed();
 
   // deploying the Protocol Contract
@@ -49,9 +49,9 @@ async function main () {
 
   // logging the addresses of the contracts
   console.log('DummyERC20 deployed to:', dummyERC20Instance.address);
-  console.log('ethvLongToken deployed to:', ethvLongToken.address);
-  console.log('ethvShortToken deployed to:', ethvShortToken.address);
-  console.log('VP deployed to:', VolmexProtocolFactoryInstance.address);
+  console.log('ETHV deployed to:', ethvLongToken.address);
+  console.log('iETHV deployed to:', ethvShortToken.address);
+  console.log('Volmex Protocol deployed to:', VolmexProtocolFactoryInstance.address);
 
   // granting MINTER_ROLE to the protocol contract
   await ethvLongToken.grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes('VOLMEX_PROTOCOL_ROLE')), VolmexProtocolFactoryInstance.address);
