c0acd4be4ebb702cafa5d9727317c0f79b00c46c
wankhede04 <wankhedevijay04@gmail.com>
2021-04-12 12:59:35 +0530

    Added required modules for upgrades.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
+require('@nomiclabs/hardhat-ethers');
+require('@openzeppelin/hardhat-upgrades');
 
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
