530ce1272302442c20c4af20fb16853386ac4f94
wankhede04 <wankhedevijay04@gmail.com>
2021-04-21 14:34:37 +0530

    Updated the test callateral token name and symbol

--- contracts/helper/TestCollateralToken.sol
+++ contracts/helper/TestCollateralToken.sol
@@ -5,7 +5,7 @@ pragma solidity =0.8.2;
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract TestCollateralToken is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("VolmexDummyERC20", "VDC") {
+    constructor() ERC20PresetMinterPauser("VolmexTestCollateralToken", "VUSD") {
         mint(msg.sender, 10000000000000000000000);
     }
 }
