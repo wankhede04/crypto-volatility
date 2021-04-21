f624148d739de8507f90c62c05facfb19aa83e74
wankhede04 <wankhedevijay04@gmail.com>
2021-04-21 14:31:22 +0530

    Strictly fixed the pragma version

--- contracts/IndexFactory.sol
+++ contracts/IndexFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.8.2;
+pragma solidity =0.8.2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
