57bf72550e1b651310be07e1a31eac50d08f95b8
wankhede04 <wankhedevijay04@gmail.com>
2021-06-03 16:25:44 +0530

    VOL-114: Added script to transafer ownership of proxy admin to gnosis safe.

---
+++ scripts/transfer-ownership.ts
@@ -0,0 +1,15 @@
+import { upgrades } from "hardhat";
+
+const transferOwnership = async () => {
+  const gnosisSafe = `${process.env.GNOSIS_SAFE_ADDRESS}`;
+
+  await upgrades.admin.transferProxyAdminOwnership(gnosisSafe);
+  console.log("Transferred ownership of ProxyAdmin to: ", gnosisSafe);
+}
+
+transferOwnership()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("Error: ", error);
+    process.exit(1);
+  });
