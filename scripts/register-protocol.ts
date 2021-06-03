7c2beb6bf3b8048c73f11c4ed5b54a0d5f012caa
wankhede04 <wankhedevijay04@gmail.com>
2021-06-03 17:06:39 +0530

    VOL-114: Added script to register protocol with different collateral

---
+++ scripts/register-protocol.ts
@@ -0,0 +1,47 @@
+import { ethers, upgrades, run } from "hardhat";
+
+const registerProtocol = async () => {
+  const VolmexIndexFactory = await ethers.getContractFactory(
+    "VolmexIndexFactory"
+  );
+
+  const VolmexProtocolFactory = await ethers.getContractFactory(
+    "VolmexProtocol"
+  );
+
+  const proxyAdmin = await upgrades.admin.getInstance();
+
+  const volmexIndexFactoryInstance = VolmexIndexFactory.attach(`${process.env.FACTORY_ADDRESS}`);
+
+  const volmexProtocolInstance = await upgrades.deployProxy(
+    VolmexProtocolFactory,
+    [
+      `${process.env.COLLATERAL_TOKEN_ADDRESS}`,
+      `${process.env.VOLATILITY_TOKEN_ADDRESS}`,
+      `${process.env.INVERSE_VOLATILITY_TOKEN_ADDRESS}`,
+      `${process.env.MINIMUM_COLLATERAL_QTY}`,
+      `${process.env.VOLATILITY_CAP_RATIO}`
+    ]
+  );
+  await volmexProtocolInstance.deployed();
+
+  console.log("Volmex Protocol Proxy deployed to: ", volmexProtocolInstance.address);
+
+  await volmexIndexFactoryInstance.registerIndex(
+    volmexProtocolInstance.address,
+    `${process.env.COLLATERAL_TOKEN_SYMBOL}`
+  );
+
+  const protocolImplementation = await proxyAdmin.getProxyImplementation(volmexProtocolInstance.address);
+
+  await run("verify:verify", {
+    address: protocolImplementation
+  });
+}
+
+registerProtocol()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("Error: ", error);
+    process.exit(1);
+  });
