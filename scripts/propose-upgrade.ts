04f80dc17df647ff240501d26a98ef325a6c9435
wankhede04 <wankhedevijay04@gmail.com>
2021-06-03 16:30:31 +0530

    VOL-114: Added script to propose upgrades.

---
+++ scripts/propose-upgrade.ts
@@ -0,0 +1,20 @@
+const { defender } = require("hardhat");
+import { ethers, run } from "hardhat";
+
+const upgrade = async () => {
+  const proxyAddress = `${process.env.PROXY_ADDRESS}`;
+
+  const VolmexProtocolV2Factory = await ethers.getContractFactory(
+    "VolmexProtocolV2"
+  );
+
+  const proposal = await defender.proposeUpgrade(proxyAddress, VolmexProtocolV2Factory);
+  console.log("Upgrade proposal created at:", proposal.url);
+}
+
+upgrade()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
