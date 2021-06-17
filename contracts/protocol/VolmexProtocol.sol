9702b3795442810868fd25b411f25df332cc07b5
wankhede04 <wankhedevijay04@gmail.com>
2021-06-17 14:53:51 +0530

    VOL-118: Made getter-setter methods virtual

--- contracts/protocol/VolmexProtocol.sol
+++ contracts/protocol/VolmexProtocol.sol
@@ -144,7 +144,7 @@ contract VolmexProtocol is
     /**
      * @notice Toggles the active variable. Restricted to only the owner of the contract.
      */
-    function toggleActive() external onlyOwner {
+    function toggleActive() external virtual onlyOwner {
         active = !active;
         emit ToggleActivated(active);
     }
@@ -155,6 +155,7 @@ contract VolmexProtocol is
      */
     function updateMinimumCollQty(uint256 _newMinimumCollQty)
         external
+        virtual
         onlyOwner
     {
         require(
@@ -173,7 +174,7 @@ contract VolmexProtocol is
     function updateVolatilityToken(
         address _positionToken,
         bool _isVolatilityIndexToken
-    ) external onlyOwner {
+    ) external virtual onlyOwner {
         _isVolatilityIndexToken
             ? volatilityToken = IERC20Modified(_positionToken)
             : inverseVolatilityToken = IERC20Modified(_positionToken);
@@ -236,6 +237,7 @@ contract VolmexProtocol is
      */
     function redeem(uint256 _positionTokenQty)
         external
+        virtual
         onlyActive
         onlyNotSettled
     {
@@ -259,7 +261,7 @@ contract VolmexProtocol is
     function redeemSettled(
         uint256 _volatilityIndexTokenQty,
         uint256 _inverseVolatilityIndexTokenQty
-    ) external onlyActive onlySettled {
+    ) external virtual onlyActive onlySettled {
         uint256 collQtyToBeRedeemed =
             (_volatilityIndexTokenQty * settlementPrice) +
                 (_inverseVolatilityIndexTokenQty *
@@ -281,6 +283,7 @@ contract VolmexProtocol is
      */
     function settle(uint256 _settlementPrice)
         external
+        virtual
         onlyOwner
         onlyNotSettled
     {
@@ -300,7 +303,7 @@ contract VolmexProtocol is
         address _token,
         address _toWhom,
         uint256 _howMuch
-    ) external nonReentrant onlyOwner {
+    ) external virtual nonReentrant onlyOwner {
         require(
             _token != address(collateral),
             "Volmex: Collateral token not allowed"
@@ -316,6 +319,7 @@ contract VolmexProtocol is
      */
     function updateFees(uint256 _issuanceFees, uint256 _redeemFees)
         external
+        virtual
         onlyOwner
     {
         require(
@@ -332,7 +336,7 @@ contract VolmexProtocol is
     /**
      * @notice Safely transfer the accumulated fees to owner
      */
-    function claimAccumulatedFees() external onlyOwner {
+    function claimAccumulatedFees() external virtual onlyOwner {
         uint256 claimedAccumulatedFees = accumulatedFees;
         delete accumulatedFees;
 
@@ -346,7 +350,7 @@ contract VolmexProtocol is
      *
      * @param _isPause Boolean value to pause or unpause the position token { true = pause, false = unpause }
      */
-    function togglePause(bool _isPause) external onlyOwner {
+    function togglePause(bool _isPause) external virtual onlyOwner {
         if (_isPause) {
             volatilityToken.pause();
             inverseVolatilityToken.pause();
